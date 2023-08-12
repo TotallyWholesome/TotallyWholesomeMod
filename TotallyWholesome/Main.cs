64fe9694612821bcb317b9edaa73c5b5195d7372
Totally Wholesome <totally@wholesome>
2023-08-12 07:51:37 +0000

    Latest changes from TotallyWholesome/TotallyWholesome from main branch

--- TotallyWholesome/Main.cs
+++ TotallyWholesome/Main.cs
@@ -20,8 +20,8 @@ namespace TotallyWholesome
         public const string Name = "TotallyWholesome";
         public const string Author = "Totally Wholesome Team";
         public const string Company = "TotallyWholesome";
-        public const string AssemblyVersion = "3.4.22";
-        public const string TWVersion = "3.4.22";
+        public const string AssemblyVersion = "3.4.23";
+        public const string TWVersion = "3.4.23";
         public const bool isBetaBuild = false;
         public const string DownloadLink = "https://totallywholeso.me/";
     }
